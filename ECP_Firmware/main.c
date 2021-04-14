c107475a4fba89223db773ad632bf25339dff5cb
Kristian Hennings <krhe@inventors.dk>
2021-04-14 12:33:05 +0200

    Created empty project

---
+++ ECP_Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * ECP_Firmware.c
+ *
+ * Created: 14-04-2021 12:31:48
+ * Author : KristianHennings
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
